d8318e7649af16b48d73d49ae31b3a88a67f4f3a
Ksenia Rybakova <rybakova.kseniia13@gmail>
2025-04-19 12:55:51 +0400

    add load discounts

--- frontend/src/app/discount/discount.component.ts
+++ frontend/src/app/discount/discount.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Discount } from '../data/models/Discount';
+import { DiscountService } from '../data/services/discount.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discount',
+  standalone: true,
   imports: [],
   templateUrl: './discount.component.html',
   styleUrl: './discount.component.css'
 })
-export class DiscountComponent {
+export class DiscountComponent implements OnInit {
+  discounts: Discount[] = [];
 
+  constructor(
+    private discountService: DiscountService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadDiscounts();
+  }
+
+  loadDiscounts(): void {
+    this.discountService.getAllDiscounts().subscribe({
+      next: (data) => this.discounts = data,
+      error: (err) => console.error('Error loading discounts', err)
+    });
+  }
 }
