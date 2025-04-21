e6581b0780e832af739cb90753fcf0f679f7dab3
Ksenia Rybakova <rybakova.kseniia13@gmail>
2025-04-21 21:29:32 +0400

    выполнена аутентификация бэка через jwt

---
+++ backend/src/main/java/com/example/lab_project/model/User.java
@@ -0,0 +1,26 @@
+package com.example.lab_project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user_table")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @Column(name = "password_user")
+    private String password;
+}
