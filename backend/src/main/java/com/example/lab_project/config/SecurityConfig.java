bd37791b76d3dca2b13a70dd2c59380d4ef15da2
Ksenia Rybakova <rybakova.kseniia13@gmail>
2025-04-21 23:17:34 +0400

    аутентификация через права

---
+++ backend/src/main/java/com/example/lab_project/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.example.lab_project.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Value("${security.logins}")
+    private String[] logins;
+    @Value("${security.passwords}")
+    private String[] passwords;
+    @Value("${security.roles}")
+    private String[] roles;
+
+    @Bean 
+    public PasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder(); 
+    } 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                // Разрешаем все GET-запросы
+                .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                
+                // Защищаем все остальные методы для ADMIN
+                .requestMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/**").hasRole("ADMIN")
+                
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        for (int i = 0; i < logins.length; i++) {
+            String login = logins[i];
+            String password = passwords[i];
+            String role = roles[i % roles.length]; 
+            UserDetails user = User.withUsername(login)
+                .password(passwordEncoder().encode(password)) 
+                .roles(role)
+                .build();
+            manager.createUser(user);
+        }
+        return manager;
+    }
+}
